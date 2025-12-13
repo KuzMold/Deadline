7d0d03f816af831b3239ccbdca9283a157fd8f69
Alex7637 <sh9y9zjvjs@privaterelay.appleid.com>
2025-12-13 19:46:19 +0400

    Без увел.

--- deadline/area_2d.gd
+++ deadline/area_2d.gd
@@ -3,9 +3,7 @@ extends Area2D
 var selected_object: Area2D = null
 var is_dragging := false
 var offset := Vector2.ZERO
-# Изменение увеличение приближения листа
-var normal_scale = Vector2(1, 1)
-var enlarged_scale = Vector2(2, 2)
+
 
 #Механика движения
 func _input (event: InputEvent):
@@ -37,33 +35,19 @@ func _input (event: InputEvent):
 @export var animation_speed: float = 0.3
 
 func _ready():
-	#Анимация поворота
+	#Функции для анимации листа
 	mouse_entered.connect(_on_mouse_entered)
 	mouse_exited.connect(_on_mouse_exited)
-	#Механика приближения
-	if self is Area2D:
-		connect("input_event", _on_input_event)
-func _on_input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton:
-		if event.pressed and event.button_index == MOUSE_BUTTON_RIGHT:
-			toggle_scale()
 	
-func toggle_scale():
-	var target_scale = enlarged_scale if scale == normal_scale else normal_scale
-	create_tween().tween_property(self, "scale", target_scale, 0.2)
-	
-	# Анимация лист поворот
 	if has_method("set_offset"):
 		offset = Vector2.ZERO
-	
-	
-# Функции для анимации листа
+
 func _on_mouse_entered():
 	var tween = create_tween()
 	tween.tween_property(animated_sprite_2d, "rotation_degrees", rotation_angle, animation_speed)\
 		.set_trans(Tween.TRANS_BACK)\
 		.set_ease(Tween.EASE_OUT)
-		
+	
 func _on_mouse_exited():
 	var tween = create_tween()
 	tween.tween_property(animated_sprite_2d, "rotation_degrees", 0.0, animation_speed)\
